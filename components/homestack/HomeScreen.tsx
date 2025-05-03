1c614d7f42afcb0ab801eaf05909ad0e03290e2a
yash2974 <yashbisht2004@gmail.com>
2025-05-03 21:11:54 +0530

    Update HomeScreen.tsx

--- components/homestack/HomeScreen.tsx
+++ components/homestack/HomeScreen.tsx
@@ -1,10 +1,21 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { GoogleSignin, User } from "@react-native-google-signin/google-signin";
 import React, { useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry";
 
 export default function HomeScreen() {
+    const getCurrentUser = async () => {
+        const currentUser = GoogleSignin.getCurrentUser();
+        if (!currentUser) {
+            console.log("No user is currently signed in.");
+            return null;
+        }
+        console.log("Current user:", currentUser);
+    };
     console.log("HomeScreen rendered");
     try {
+        getCurrentUser();
         const token = AsyncStorage.getItem("userToken");
         if (token) {
             console.log("Token found:", token);
@@ -35,3 +46,7 @@ const styles = StyleSheet.create({
         marginBottom: 20,
     },
 });
+
+function setState(currentUser: User | null) {
+    throw new Error("Function not implemented.");
+}
